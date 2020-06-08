d608d999b88febb6f70597b214cdbfff5d171954
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-06-08 17:29:13 +0200

    feat: time display in proper timezone for search

--- src/renderWeatherUnit.js
+++ src/renderWeatherUnit.js
@@ -6,11 +6,12 @@ import getWeatherData from './getWeatherData';
 
 const celToFarConverter = (celcius) => celcius * 1.8 + 32;
 
-const renderTimeandLocation = (city, country, lang) => {
+const renderTimeandLocation = (city, country, lang, timezone) => {
   const now = moment();
-  now.locale(lang);
+  // now.locale(lang);
+  console.log()
   document.querySelector('#location').textContent = `${city}, ${country}`;
-  document.querySelector('#time').textContent = now.format('ddd DD MMMM HH:mm');
+  document.querySelector('#time').textContent = now.utcOffset(timezone / 60).locale(lang).format('ddd DD MMMM HH:mm');
 };
 
 const renderCurrentData = (current, tempUnit, lang) => {
@@ -39,8 +40,8 @@ export default async (search) => {
   const unit = localStorage.getItem('unit') ? localStorage.getItem('unit') : 'celcius';
   const lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : 'en';
   const { city, country } = await getCity();
-  const { current, daily } = await getWeatherData(search, lang);
-  renderTimeandLocation(city, country, lang);
+  const { current, daily, timezone_offset } = await getWeatherData(search, lang);
+  renderTimeandLocation(city, country, lang, timezone_offset);
   renderCurrentData(current, unit, lang);
   renderFutureForecast(daily, unit, lang);
 };
